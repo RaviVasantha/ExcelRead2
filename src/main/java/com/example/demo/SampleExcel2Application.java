18aa080a9a588860d08620db99e8b9083fa16e3a
Ravivasantha <bravibharathivj@gmail.com>
2024-04-23 12:18:27 +0530

    first commit

---
+++ src/main/java/com/example/demo/SampleExcel2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleExcel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleExcel2Application.class, args);
+	}
+
+}
